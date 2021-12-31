af7ff23c59cf7152bcb6c61c9465fcfd692b577f
acepnurmansidik <acepnurmansidik@gmail.com>
2021-12-31 08:45:46 +0700

    new setup db

--- config/db.js
+++ config/db.js
@@ -5,7 +5,6 @@ const dbPool = new Pool({
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
 });
 
 module.exports = dbPool;

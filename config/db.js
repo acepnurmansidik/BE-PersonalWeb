cbc11db1548b18f8cfc22c31af88749ef2c3dfef
acepnurmansidik <acepnurmansidik@gmail.com>
2021-12-31 08:11:03 +0700

    setup db using env

--- config/db.js
+++ config/db.js
@@ -1,14 +1,11 @@
 const { Pool } = require("pg");
 
-// const dbPool = new Pool({
-//   database: "personal_web",
-//   port: 5432,
-//   user: "postgres",
-//   password: "root",
-// });
-
 const dbPool = new Pool({
-  database: process.env.DATABASE_URL,
+  database: process.env.DB_URL,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
 });
 
 module.exports = dbPool;
